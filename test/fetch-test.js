dbcbe708002af534facb43fcb60a81081768e00b
Milan Simonovic <mbsimonovic@gmail.com>
2023-08-29 09:11:07 +0200

    prep test for future conflicts

--- test/fetch-test.js
+++ test/fetch-test.js
@@ -7,14 +7,10 @@ var fetchWeather = require("../fetch-weather");
 var rawWeatherData = require("../test-data/sample-weather-raw.json");
 
 describe("Fetching data from OpenWeatherMap", function () {
-  it("Should convert Fahrenheit to Celsius", async function () {
+  it("Should convert temp from K to C", async function () {
     sinon.stub(axios, "get").resolves({ data: rawWeatherData });
     const results = await fetchWeather.fetchWeather("London");
-    expect(results).to.deep.equal({
-      maxTemp: 17,
-      minTemp: 14,
-      rainFall: 2.29,
-      cloudCover: 40,
-    });
+    expect(results.maxTemp).to.equal(17);
+    expect(results.minTemp).to.equal(14);
   });
 });
